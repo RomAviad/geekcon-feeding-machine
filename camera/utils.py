7da9967b2f0ac04b5e7b00818cf4fac21055da03
Aviad Rom <aviad@zebra-med.com>
2019-09-20 15:31:19 +0300

    fix x,y flip in mask boundary

--- camera/utils.py
+++ camera/utils.py
@@ -11,10 +11,10 @@ def calculate_naive_mask_bounding_box(img):
     min_x, min_y, max_x, max_y = 0, 0, 0, 0
     non_zeros = np.nonzero(img)
     if np.any(non_zeros):
-        min_x = non_zeros[0].min()
-        max_x = non_zeros[0].max()
-        min_y = non_zeros[1].min()
-        max_y = non_zeros[1].max()
+        min_x = non_zeros[1].min()
+        max_x = non_zeros[1].max()
+        min_y = non_zeros[0].min()
+        max_y = non_zeros[0].max()
 
     return min_x, min_y, max_x, max_y
 
